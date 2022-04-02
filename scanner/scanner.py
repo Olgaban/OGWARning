17b0e7fb1d84debb58aafde040989bff8a577d0c
Szymon-Budziak <szymonbudziak@gmail.com>
2022-04-02 23:20:46 +0200

    code refactor

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,5 +1,4 @@
 import asyncio
-import struct
 from bleak import BleakScanner
 
 timeout_seconds = 30
@@ -10,13 +9,9 @@ service_id_to_look_for = 'masked'
 def detection_callback(device, advertisement_data):
     for some_id in advertisement_data.service_data:
         message = advertisement_data.service_data.get(some_id)
-        # print(some_id)
-        # num_to_test = struct.unpack_from('<I', message, 0)
         if some_id != "0000feaa-0000-1000-8000-00805f9b34fb":
             continue
         print(message)
-        # print(device.address, "RSSI:", device.rssi, advertisement_data)
-        # print(device.address, advertisement_data.service_data)
 
 
 async def run():
